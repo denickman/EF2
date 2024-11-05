8c5753c0fde5113a83145e0f225984cdac7bc83c
Dennis Yaremenko <yaremdennis@gmail.com>
2024-11-05 14:46:57 +0200

    Create a feed loader protocol

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,19 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Denis Yaremenko on 05.11.2024.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
