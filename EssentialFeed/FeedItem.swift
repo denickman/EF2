6caad216671a8ca5bd36145ab3d124ddaeafa9c1
Dennis Yaremenko <yaremdennis@gmail.com>
2024-11-05 14:43:32 +0200

    Create EssentialFeed framework, create feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Denis Yaremenko on 05.11.2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
