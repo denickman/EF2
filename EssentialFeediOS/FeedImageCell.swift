3e9a84a8c2166a7d7bfb7c4c5fab1495c6c8ec1c
Dennis Yaremenko <yaremdennis@gmail.com>
2024-12-06 11:44:43 +0200

    Effectively Test-driving MVC UI with Multiple Views/Models/State, Efficiently Loading/Prefetching/Cancelling Image Requests, Inside-Out vs. Outside-In Development, and Identifying the Massive View Controller Anti-pattern by Following Design Principles

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,30 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed
+//
+//  Created by Denis Yaremenko on 05.12.2024.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    public let feedImageContainer = UIView()
+    public let feedImageView = UIImageView()
+
+    private(set) public lazy var feedImageRetryButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    var onRetry: (() -> Void)?
+
+    @objc private func retryButtonTapped() {
+        onRetry?()
+    }
+}
+
