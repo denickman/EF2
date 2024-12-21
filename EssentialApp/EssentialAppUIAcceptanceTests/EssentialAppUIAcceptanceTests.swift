91564183a4552737133d4d3f42239d246b61bbed
Dennis Yaremenko <yaremdennis@gmail.com>
2024-12-21 16:33:23 +0200

    Refactor unit tests

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -10,47 +10,37 @@ import XCTest
 final class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
-        let app = XCUIApplication()
-        app.launchArguments = ["-reset", "-connectivity", "online"]
-        app.launch()
-        
+             let app = XCUIApplication()
+             app.launchArguments = ["-reset", "-connectivity", "online"]
+             app.launch()
+
+             let feedCells = app.cells.matching(identifier: "feed-image-cell")
         // private func makeFeedData() -> Data in DebuggingSceneDelegate
-        XCTAssertEqual(app.cells.count, 2)
-        
-//        let firstCell = app.cells.firstMatch
-//        let image = firstCell.images.matching(identifier: "feed-image").firstMatch
-//        
-//        XCTAssertTrue(firstCell.exists, "First cell should exist")
-//        XCTAssertTrue(image.exists, "Image in the first cell should exist")
-        
-        let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        // private func makeFeedData() -> Data in DebuggingSceneDelegate
-        XCTAssertEqual(feedCells.count, 2)
-        
-        let firstImage = app.images.matching(identifier: "feed-image").firstMatch
-        XCTAssertTrue(firstImage.exists)
-    }
+             XCTAssertEqual(feedCells.count, 2)
+
+             let firstImage = app.images.matching(identifier: "feed-image").firstMatch
+             XCTAssertTrue(firstImage.exists)
+         }
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         // first we have to launch the app with connectivity in order to store feed into the cache
         // then launch it offline to check the cache is exist
-        let onlineApp = XCUIApplication()
-        onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
-        onlineApp.launch()
-        
-        let offlineApp = XCUIApplication()
-        
-        /// Передача флага через launchArguments: Когда вы вызываете offlineApp.launchArguments = ["-connectivity", "offline"] перед запуском приложения, система автоматически добавляет этот аргумент в UserDefaults.
-        offlineApp.launchArguments = ["-connectivity", "offline"]
-        offlineApp.launch()
+             let onlineApp = XCUIApplication()
+             onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
+             onlineApp.launch()
 
-        let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(cachedFeedCells.count, 2)
-        
-        let firstCachedImage = offlineApp.images.matching(identifier: "feed-image").firstMatch
-        XCTAssertTrue(firstCachedImage.exists)
-    }
-    
+             let offlineApp = XCUIApplication()
+        /// Передача флага через launchArguments: Когда вы вызываете offlineApp.launchArguments = ["-connectivity", "offline"] перед запуском приложения, система автоматически добавляет этот аргумент в UserDefaults.
+             offlineApp.launchArguments = ["-connectivity", "offline"]
+             offlineApp.launch()
+
+             let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+             XCTAssertEqual(cachedFeedCells.count, 2)
+
+             let firstCachedImage = offlineApp.images.matching(identifier: "feed-image").firstMatch
+             XCTAssertTrue(firstCachedImage.exists)
+         }
+
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
         // no connectivity and no cache
         
@@ -58,7 +48,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         app.launchArguments = ["-reset", "-connectivity", "offline"]
         app.launch()
         
-        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        let feedCells = app.cells.matching(identifier: "feed-image")
         XCTAssertEqual(feedCells.count, .zero)    
     }
     
