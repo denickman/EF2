3d5754ef3944188e1a5f686c0b6bb27802c00bdc
Dennis Yaremenko <yaremdennis@gmail.com>
2024-12-23 11:04:26 +0200

    Record feed with content snapshot

--- EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -8,21 +8,27 @@
 import XCTest
 import EssentialApp
 import EssentialFeediOS
+@testable import EssentialFeed // in order to not create public init for FeedImageViewModel
+
 
 class FeedSnapshotTests: XCTestCase {
     
     func test_emptyFeed() {
         let sut = makeSUT()
-
         sut.display(emptyFeed())
         let snapshot = sut.snapshot()
         record(snapshot: snapshot, named: "EMPTY_FEED")
     }
     
+    func test_feedWithContent() {
+        let sut = makeSUT()
+        sut.display(feedWithContent())
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
+    }
     
     // MARK: - Helpers
     
-    func makeSUT() -> FeedViewController {
+    private func makeSUT() -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let controller = storyboard.instantiateInitialViewController() as! FeedViewController
@@ -34,6 +40,21 @@ class FeedSnapshotTests: XCTestCase {
         []
     }
     
+    private func feedWithContent() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+    
     private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
         guard let snapshotData = snapshot.pngData() else {
             XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
@@ -41,7 +62,7 @@ class FeedSnapshotTests: XCTestCase {
         }
         
         // ../EssentialFeediOSTests/FeedSnapshotTests.swift
-
+        
         let snapshotURL = URL(fileURLWithPath: String(describing: file))
             .deletingLastPathComponent() // ../EssentialFeediOSTests/
             .appendingPathComponent("snapshots")  // ../EssentialFeediOSTests/snapshots
@@ -59,7 +80,6 @@ class FeedSnapshotTests: XCTestCase {
     }
 }
 
-
 extension UIViewController {
     func snapshot() -> UIImage {
         let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
@@ -67,4 +87,40 @@ extension UIViewController {
             view.layer.render(in: action.cgContext)
         }
     }
+}
+
+private extension FeedViewController {
+    func display(_ stubs: [ImageStub]) {
+        // convert image stubs into cell controllers
+        let cells: [FeedImageCellController] = stubs.map { stub in
+            let cellController = FeedImageCellController(delegate: stub)
+            stub.controller = cellController
+            return cellController
+        }
+        
+        display(cells)
+    }
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+    
+    let viewModel: FeedImageViewModel<UIImage>
+    weak var controller: FeedImageCellController?
+    
+    init(description: String?, location: String?, image: UIImage?) {
+        viewModel = FeedImageViewModel(
+            description: description,
+            location: location,
+            image: image,
+            isLoading: false,
+            shouldRetry: image == nil
+        )
+    }
+    
+    func didRequestImage() {
+        // when the ctrl requests an image, we need to tell the ctrl to display a stub view model
+        controller?.display(viewModel)
+    }
+    
+    func didCancelImageRequest() {}
 }
