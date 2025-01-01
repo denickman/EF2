3c839a0bdcdf73007cce7319e8c0fb4b31b34b23
Dennis Yaremenko <yaremdennis@gmail.com>
2025-01-01 11:45:06 +0200

    Test image comments in isolation

--- EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
+++ EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
@@ -10,81 +10,59 @@ import EssentialFeed
 
 final class ImageCommentsMapperTests: XCTestCase {
     
-    func test_load_deliversErrorOnNon2xxHTTPResponse() throws {
+    func test_map_throwsErrorOnNon2xxHTTPResponse() throws {
         // check 199, 201, 300, 400, 500 statuses
         
-        let (client, sut) = makeSut()
         let samples = [150, 199, 300, 400, 500]
         let json = makeItemsJSON([])
         
         try samples.forEach { code in
-           XCTAssertThrowsError(
-            try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(json, from: .init(statusCode: code))
             )
         }
     }
     
     // response 200 but invalid data
-    func test_load_deliversErrorOn2xxHTTPResponseWithInvalidJSON() {
+    func test_map_throwsErrorOn2xxHTTPResponseWithInvalidJSON() throws {
         
-        let (client, sut) = makeSut()
         let samples = [200, 201, 250, 280, 299]
+        let invalidJSON = Data("invalidJson".utf8)
         
-        samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithResult: failure(.invalidData), when: {
-                let invalidJSON = Data("invalidJson".utf8)
-                client.complete(withStatusCode: code, data: invalidJSON, at: index)
-            })
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(invalidJSON, from: .init(statusCode: code))
+            )
         }
     }
     
-    func test_load_deliversNoItemsOn2xxHTTPResponseWithEmptyList() {
-        let (client, sut) = makeSut()
+    func test_map_deliversNoItemsOn2xxHTTPResponseWithEmptyList() throws {
         let samples = [200, 201, 250, 280, 299]
+        let emptyListJSON = makeItemsJSON([]) // Data("{\"items\":[]}".utf8)
         
-        samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithResult: .success([])) {
-                let emptyListJSON = makeItemsJSON([]) // Data("{\"items\":[]}".utf8)
-                client.complete(withStatusCode: code, data: emptyListJSON, at: index)
-            }
+        try samples.forEach { code in
+            let result = try ImageCommentsMapper.map(emptyListJSON, from: .init(statusCode: code))
+            XCTAssertEqual(result, [])
         }
     }
     
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "https://any-url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteImageCommentsLoader? = RemoteImageCommentsLoader(url: url, client: client)
+    func test_map_deliversItemsOn2xxHTTPResponseWithJSONItems() throws  {
         
-        var capturedResults = [RemoteImageCommentsLoader.Result]()
+        let item1 = makeItem(id: UUID(), message: "a message", createdAt: (Date(timeIntervalSince1970: 1598627222), "2020-08-28T15:07:02+00:00"), username: "a username")
         
-        sut?.load {
-            capturedResults.append($0)
+        let item2 = makeItem(id: UUID(), message: "another message", createdAt: (Date(timeIntervalSince1970: 1577881882), "2020-01-01T12:31:22+00:00"), username: "a username")
+        
+        let json = makeItemsJSON([item1.json, item2.json])
+        let samples = [200, 201, 250, 280, 299]
+        
+        try samples.forEach { code in
+            let result = try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            XCTAssertEqual(result, [item1.model, item2.model])
         }
         
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-        XCTAssertTrue(capturedResults.isEmpty)
     }
     
     // MARK: - Helpers
-    
-    private func makeSut(
-        url: URL = URL(string: "https://any-url.com")!,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) -> (client: HTTPClientSpy, sut: RemoteImageCommentsLoader) {
-        let client = HTTPClientSpy()
-        let sut = RemoteImageCommentsLoader(url: url, client: client)
-        
-        trackForMemoryLeaks(client, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        
-        return (client, sut)
-    }
-    
-    private func failure(_ error: RemoteImageCommentsLoader.Error) -> RemoteImageCommentsLoader.Result {
-        .failure(error)
-    }
     
     private func makeItem(
         id: UUID,
@@ -105,39 +83,6 @@ final class ImageCommentsMapperTests: XCTestCase {
         ]
         
         return (item, json)
-    }
-    
-    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["items" : items]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
-    
-    private func expect(
-        _ sut: RemoteImageCommentsLoader,
-        toCompleteWithResult expected: RemoteImageCommentsLoader.Result,
-        when action: () -> Void,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load { received in
-            switch (received, expected) {
-            case let (.success(receivedItems), .success(expectedItems)):
-                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-                
-            case (.failure(let receivedError as RemoteImageCommentsLoader.Error), .failure(let expectedError as RemoteImageCommentsLoader.Error)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-                
-            default:
-                XCTFail("Expected result \(expected) got \(received) instead", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-        
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
     }
     
 }
