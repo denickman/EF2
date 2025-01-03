79d0c09f2a06a5c9ad1596dea35284d25dc4046a
Dennis Yaremenko <yaremdennis@gmail.com>
2025-01-03 14:47:07 +0200

    Display error on mapper error

--- EssentialFeediOS/FeedUI/Controllers/FeedImageCellController.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedImageCellController.swift
@@ -13,20 +13,29 @@ public protocol FeedImageCellControllerDelegate {
     func didCancelImageRequest()
 }
 
-public final class FeedImageCellController: FeedImageView {
+public final class FeedImageCellController: FeedImageView, ResourceView, ResourceLoadingView, ResourceErrorView {
     
+    public typealias ResourceViewModel = UIImage
+    
+    private let viewModel: FeedImageViewModel<UIImage>
     private let delegate: FeedImageCellControllerDelegate
     private var cell: FeedImageCell?
     
-    public init(delegate: FeedImageCellControllerDelegate) {
+    public init(viewModel: FeedImageViewModel<UIImage>, delegate: FeedImageCellControllerDelegate) {
+        self.viewModel = viewModel
         self.delegate = delegate
     }
     
     func view(in tableView: UITableView) -> UITableViewCell {
-        self.cell = tableView.dequeueReusableCell()
+        cell = tableView.dequeueReusableCell()
+        
+        cell?.locationContainer.isHidden = !viewModel.hasLocation
+        cell?.locationLabel.text = viewModel.location
+        cell?.descriptionLabel.text = viewModel.description
+        
         /// accessibilityIdentifier 'feed-image-cell' for EssentialAppUIAcceptanceTests
         self.cell?.accessibilityIdentifier = "feed-image-cell"
-
+        
         delegate.didRequestImage()
         return cell!
     }
@@ -41,23 +50,28 @@ public final class FeedImageCellController: FeedImageView {
     }
     
     public func display(_ viewModel: FeedImageViewModel<UIImage>) {
-        
-        cell?.locationContainer.isHidden = !viewModel.hasLocation
-        cell?.locationLabel.text = viewModel.location
-        cell?.descriptionLabel.text = viewModel.description
         /// accessibilityIdentifier 'feed-image' for EssentialAppUIAcceptanceTests
         cell?.feedImageView.accessibilityIdentifier = "feed-image"
         cell?.feedImageView.image = viewModel.image
-        
-        cell?.feedImageView.setImageAnimated(viewModel.image)
-        
-        cell?.feedImageContainer.isShimmering = viewModel.isLoading
-        cell?.feedImageRetryButton.isHidden = !viewModel.shouldRetry
         cell?.onRetry = delegate.didRequestImage
     }
     
     func releaseCellForReuse() {
         cell = nil
     }
+    
+    // MARK: - ResourceView Protocol
+    public func display(_ viewModel: UIImage) {
+        cell?.feedImageView.setImageAnimated(viewModel)
+    }
+
+    public func display(_ viewModel: ResourceLoadingViewModel) {
+        cell?.feedImageContainer.isShimmering = viewModel.isLoading
+    }
+    
+    public func display(_ viewModel: ResourceErrorViewModel) {
+        cell?.feedImageRetryButton.isHidden = viewModel.message == nil
+    }
+    
 }
 
