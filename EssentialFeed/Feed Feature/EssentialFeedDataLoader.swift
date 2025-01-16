b422f370a6fb56aabb5ab8297eaee64ae40a3455
Dennis Yaremenko <yaremdennis@gmail.com>
2025-01-16 17:47:48 +0200

    Make CoreData FeedImageDataStore implementation sync

--- EssentialFeed/Feed Feature/EssentialFeedDataLoader.swift
+++ EssentialFeed/Feed Feature/EssentialFeedDataLoader.swift
@@ -5,12 +5,7 @@
 //  Created by Denis Yaremenko on 16.12.2024.
 //
 
-public protocol FeedImageDataLoaderTask {
-    func cancel()
-}
-// protocol defines to load image data
+
 public protocol FeedImageDataLoader {
-    typealias Result = Swift.Result<Data, Error>
-    
-    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+    func loadImageData(from url: URL) throws -> Data
 }
